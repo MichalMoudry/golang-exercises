f33c5a526d5473db8fe23665500c0e3ef5b0c6ea
Michal Moudrý <moudry.michal@live.com>
2022-06-27 21:09:58 +0200

    Finished string conversion

--- book-exercises/1-chapter/04-converting.go
+++ book-exercises/1-chapter/04-converting.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
+func main() {
+	if len(os.Args) >= 2 {
+		arg := os.Args[1]
+		fmt.Printf("Argument 1: %s\n", arg)
+		fmt.Printf("Argument 1 type: %T\n", arg)
+		no1, err := strconv.Atoi(arg)
+		if err != nil {
+			panic("Couldn't convert to number.")
+		} else {
+			println("10 +", no1, "=", 10+no1)
+		}
+	} else {
+		println("No arguments were added")
+	}
 }
