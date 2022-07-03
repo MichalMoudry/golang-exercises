a1958345f1895034a8ab6a014e9980707d9ac7e5
Michal Moudrý <moudry.michal@live.com>
2022-07-03 16:46:19 +0200

    Finished array exercise

--- book-exercises/2-chapter/01-arrays.go
+++ book-exercises/2-chapter/01-arrays.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	// create array
+	var commands []string
+
+	var response string
+	for {
+		fmt.Print("command> ")
+		fmt.Scan(&response)
+		if response == "quit" {
+			break
+		} else if response == "new" {
+			fmt.Print("Entry:")
+			fmt.Scan(&response)
+			commands = append(commands, response)
+			// save entry to list
+			fmt.Println("Saving entry")
+		} else if response == "list" {
+			// list entries
+			fmt.Println("Listing entries")
+			for i := 0; i < len(commands); i++ {
+				println(commands[i])
+			}
+		} else {
+			fmt.Println("Unknown command", response)
+		}
+
+	}
+	fmt.Println("bye")
 }
