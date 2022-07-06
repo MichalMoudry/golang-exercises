25f798841d9067cb87437c977c415be4325043e5
Michal Moudrý <moudry.michal@live.com>
2022-07-06 15:39:54 +0200

    Finished comments exercise

--- exercism-exercises/comments/comments.go
+++ exercism-exercises/comments/comments.go
@@ -1,4 +1,17 @@
+// Package containing solution to Weather Forecast exercise.
 package main
+
+// CurrentCondition represents current condition of a specific location.
+var CurrentCondition string
+
+// CurrentLocation represents current location.
+var CurrentLocation string
+
+// Forecast() return forcast for a specific location.
+func Forecast(city, condition string) string {
+	CurrentLocation, CurrentCondition = city, condition
+	return CurrentLocation + " - current weather condition: " + CurrentCondition
+}
 
 func main() {
 
