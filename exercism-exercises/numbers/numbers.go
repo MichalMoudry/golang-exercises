aaab585dfdfb08c102028be8298a1e600294f4ca
Michal Moudrý <moudry.michal@live.com>
2022-07-06 16:08:27 +0200

    Finished numbers exercise

--- exercism-exercises/numbers/numbers.go
+++ exercism-exercises/numbers/numbers.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	return (float64(productionRate) / successRate) / 100
+}
 
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	var perHour float64 = CalculateWorkingCarsPerHour(productionRate, successRate)
+	return int(perHour) / 60
+}
+
+// CalculateCost works out the cost of producing the given number of cars
+func CalculateCost(carsCount int) uint {
+	var surplus int = carsCount % 10
+	var pairs int = (carsCount - surplus) / 10
+	return uint(pairs)*95000 + uint(surplus)*10000
+}
+
+func main() {
+	println(CalculateWorkingCarsPerHour(1547, 90))
+	println(CalculateWorkingCarsPerHour(221, 100))
+	println(CalculateWorkingCarsPerMinute(1105, 90))
+	println(CalculateCost(37))
 }
