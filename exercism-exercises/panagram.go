def098ccaeef0cb855a8d0c89b19399c7f8ca75a
Michal Moudrý <moudry.michal@live.com>
2022-07-31 17:19:22 +0200

    Finished some exercises

---
+++ exercism-exercises/panagram.go
@@ -0,0 +1,23 @@
+package main
+
+import "strings"
+
+func IsPangram(input string) bool {
+	input = strings.ToLower(input)
+	input = strings.ReplaceAll(input, " ", "")
+	input = strings.ReplaceAll(input, ",", "")
+	input = strings.TrimRight(input, ".")
+	for i := 'a'; i <= 'z'; i++ {
+		if strings.IndexRune(input, i) == -1 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	println("Is panagram (expected - true):", IsPangram("The quick brown fox jumps over the lazy dog."))
+	println("Is panagram (expected - false):", IsPangram("a quick movement of the enemy will jeopardize five gunboats"))
+	println("Is panagram (expected - false):", IsPangram("five boxing wizards jump quickly at it"))
+	println("Is panagram (expected - false):", IsPangram("7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog"))
+}
