ceaf083cbc2b9e0dc73a86df78630367c80cfe44
Michal Moudrý <moudry.michal@live.com>
2023-12-08 12:42:41 +0100

    Finished collatz conjecture

---
+++ exercism-exercises/collatz_conjecture.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	NegativeNumberErr = errors.New("only positive numbers are allowed")
+)
+
+func CollatzConjecture(n int) (int, error) {
+	if n <= 0 {
+		return 0, NegativeNumberErr
+	}
+
+	var steps int
+	for n != 1 {
+		if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = n*3 + 1
+		}
+		steps += 1
+	}
+	return steps, nil
+}
+
+func main() {
+	res1, err1 := CollatzConjecture(1000000)
+	fmt.Println(res1, "|", err1)
+}
