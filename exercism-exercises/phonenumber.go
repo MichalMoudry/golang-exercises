558fc24c9e968e2015fc6ec1a588502bee788d8a
Michal Moudrý <moudry.michal@live.com>
2023-12-09 20:49:11 +0100

    Started phone number exercise

---
+++ exercism-exercises/phonenumber.go
@@ -0,0 +1,17 @@
+package main
+
+func Number(phoneNumber string) (string, error) {
+	panic("Please implement the Number function")
+}
+
+func AreaCode(phoneNumber string) (string, error) {
+	panic("Please implement the AreaCode function")
+}
+
+func Format(phoneNumber string) (string, error) {
+	panic("Please implement the Format function")
+}
+
+func main() {
+
+}
